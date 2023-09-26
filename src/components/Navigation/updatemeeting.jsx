fd904770640b98513f6548a9c67fd0e629fb71a2
sobol <nppstcc1001050311@gmail.com>
2023-09-26 21:07:41 +0800

    實作修改會議

---
+++ src/components/Navigation/updatemeeting.jsx
@@ -0,0 +1,205 @@
+import React, { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useIsLoginStore } from '../../store/useIsLoginStore';
+import './updatemeeting.css'; // 導入CSS
+
+// yyyy-MM-ddThh:mm
+// reference -> https://stackoverflow.com/questions/30166338/setting-value-of-datetime-local-from-date
+Date.prototype.toDatetimeLocalString = function () {
+    // return new Date(this.getTime() + new Date().getTimezoneOffset() * -60 * 1000).toISOString().slice(0, 19)
+    this.setMinutes(this.getMinutes()-this.getTimezoneOffset())
+    return this.toISOString().slice(0,16)
+}
+
+
+const UpdateMeeting = () => {
+    const [name, setname] = useState('');
+    const [start, setstart] = useState('');
+    const [end, setend] = useState('');
+    const [meetingRoomId, setmeetingRoomId] = useState('');
+    const [creatorId, setcreatorId] = useState('');
+    const [notificationTime, setNotificationTime] = useState(0);
+
+    const { isLogin, setIsLogin } = useIsLoginStore();
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    //會議通知radio
+    const [announcement, setAnnouncement] = useState('');
+    const onOptionChange = function (e) {
+        console.log(e.target.value)
+        setAnnouncement(e.target.value)
+    }
+
+    const [meetingRooms, setmeetingRooms] = useState([]);
+    useEffect(() => {
+        if (location.state.notificationTime && location.state.notificationTime > 0) {
+            setAnnouncement('Open')
+            setNotificationTime(location.state.notificationTime)
+        }
+
+        setname(location.state.name)
+        setstart(new Date(location.state.start).toDatetimeLocalString())
+        setend(new Date(location.state.end).toDatetimeLocalString())
+        setcreatorId(location.state.creatorId)
+        setmeetingRoomId(location.state.meetingRoomId)
+
+
+        fetch('http://localhost:5000/meeting-room', {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                'Authorization': `Bearer ${localStorage.getItem('jwtToken')}`
+            }
+        })
+            .then(res => res.json())
+            .then(res => {
+                if (res.status) {
+                    if (res.status === 200) {
+                        setmeetingRooms(res.data);
+                    }
+                }
+
+                if (res.statusCode) {
+                    if (res.statusCode === 401) {
+                        alert("請重新登入！")
+                        localStorage.removeItem("jwtToken")
+                        localStorage.removeItem('userid')
+                        setIsLogin(false)
+                        navigate('/')
+                    }
+                }
+            })
+            .catch(err => {
+            })
+    }, [])
+
+
+    useEffect(() => {
+        if (announcement==='') {
+            return
+        }
+
+        if (announcement !== 'Open') {
+            setNotificationTime(0)
+        }
+        console.log(announcement,notificationTime)
+    }, [announcement,notificationTime])
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const create = {
+            name,
+            start: (new Date(start)).toISOString(),
+            end: (new Date(end)).toISOString(),
+            meetingRoomId,
+            creatorId,
+            notificationTime
+        };
+
+        fetch(`http://localhost:5000/meeting/${location.state.id}`, {
+            method: 'PUT',
+            body: JSON.stringify(create),
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                'Authorization': `Bearer ${localStorage.getItem('jwtToken')}`
+            }
+        })
+            .then(res => res.json())
+            .then(res => {
+                switch (res.status) {
+                    case 200:
+                        alert('修改會議成功!')
+                        navigate('/meeting')
+                        break;
+                    default:
+                        console.log(res)
+                        alert(res.message)
+                        break;
+                }
+            })
+    }
+
+
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className="createmeeting">
+                <h2>更新會議</h2>
+                <div className='createmeeting_row'>
+                    <div>
+                        <label><b>會議名稱：</b></label>
+                        <input type="text" value={name} onChange={(e) => setname(e.target.value)} />
+                    </div>
+                </div>
+                <div className='createmeeting_row'>
+                    <div className='announcement-container'>
+                        <label><b>會議通知：</b></label>
+                        <input type="radio" name="announcement" value="Open" id="open" checked={announcement === "Open"} onChange={onOptionChange} />
+                        <label htmlFor='open'>開啟</label>
+                        <input type="radio" name="announcement" value="Close" id="close" checked={announcement === "Close"} onChange={onOptionChange} />
+                        <label htmlFor='close'>關閉</label>
+                        {announcement === 'Open' && (
+                            <select
+                                value={notificationTime}
+                                onChange={(e) => {
+                                    setNotificationTime(parseInt(e.target.value))
+                                }}
+                                className="custom-select"
+                                id="shopSearchSelect"
+                            >
+                                <option value={0}>請選擇時間</option>
+                                <option value={5}>5分鐘</option>
+                                <option value={10}>10分鐘</option>
+                                <option value={15}>15分鐘</option>
+                                <option value={30}>30分鐘</option>
+                            </select>
+                        )}
+                    </div>
+                </div>
+
+                <div className='createmeeting_row'>
+                    <div className='meeting_start_time'>
+                        <label><b>會議開始時間：</b></label>
+                        <input type="datetime-local" value={start} onChange={(e) => setstart(e.target.value)}
+
+                        />
+
+                    </div>
+                </div>
+                <div className='createmeeting_row'>
+                    <div className='meeting_end_time'>
+                        <label><b>會議結束時間：</b></label>
+                        <input type="datetime-local" value={end} onChange={(e) => setend(e.target.value)} />
+                    </div>
+                </div>
+                <div className='createmeeting_row'>
+                    <div>
+                        <label><b>會議地點：</b></label>
+                        <select className="custom-select" id="shopSearchSelect" value={meetingRoomId} onChange={(e) => setmeetingRoomId(e.target.value)}>
+                            <option value=''>請選擇會議地點</option>
+                            {
+                                meetingRooms.map(meetingRoom => {
+                                    return <option key={meetingRoom.id} value={meetingRoom.id}>{meetingRoom.name}</option>
+                                })
+                            }
+                        </select>
+                    </div>
+                </div>
+                <div className='createmeeting_row'>
+                    <button type="create"><b>更新</b></button>
+                </div>
+            </div>
+        </form>
+    );
+};
+
+export default UpdateMeeting;
+
+
+
+
+

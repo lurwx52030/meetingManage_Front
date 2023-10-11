ea58831b01f8e71cd42c20de7dc595ccc70e234a
sobol <C109152304@nkust.edu.tw>
2023-10-11 23:46:55 +0800

    update eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,6 +16,7 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
-    "no-unused-vars":"off",
+    "no-unused-vars": "off",
+    "no-case-declarations": "off",
   },
 }

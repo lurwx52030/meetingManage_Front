9e0c4e5ff21e39d9bf650eb0cb688dd91be6c99d
sobol <C109152304@nkust.edu.tw>
2023-11-01 20:18:36 +0800

    確實把未簽到簽退之備註寫到資料庫

--- src/components/modals/RemarkModal.jsx
+++ src/components/modals/RemarkModal.jsx
@@ -1,13 +1,67 @@
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BsFillChatLeftTextFill } from "react-icons/bs";
+import { useLocation, useNavigate } from 'react-router-dom';
 import { Button, Modal } from 'semantic-ui-react';
+import { useIsLoginStore } from '../../store/useIsLoginStore';
 
-function RemarkModal({ meeting }) {
+function RemarkModal({ meetingId, participant }) {
     const [isRemark, setIsRemark] = useState(false);
+    const [remark, setRemark] = useState('');
 
-    // useEffect(()=>{console.log(meeting)},[])
+    const { isLogin, setIsLogin } = useIsLoginStore();
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    useEffect(() => {
+        console.log(participant);
+        // Nullish Coalescing Operator
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing
+        const r = participant.remark ?? ''
+        setRemark(r)
+    }, [participant]);
+
+    const setRemarkHandler = () => {
+        const p = { meetingId, employeeId: participant.id, remark }
+
+        fetch(`http://localhost:5000/meeting-member/remark`, {
+            method: 'PUT',
+            body: JSON.stringify(p),
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                'Authorization': `Bearer ${localStorage.getItem('jwtToken')}`
+            }
+        })
+            .then(res => res.json())
+            .then(res => {
+                switch (res.status) {
+                    case 200:
+                        alert('備註更新成功！');
+                        setIsRemark(false);
+                        break;
+                    default:
+                        return Promise.reject(res);
+                }
+            })
+            .catch(error => {
+                switch (error.statusCode) {
+                    case 401:
+                        localStorage.removeItem("jwtToken")
+                        localStorage.removeItem('userid')
+                        alert("請重新登入！")
+                        navigate("/")
+                        setIsLogin(false)
+                        break;
+                    case 403:
+                        alert("您沒有權限！")
+                        navigate("/meeting");
+                        break;
+                }
+            })
+    }
 
     return (
         <Modal
@@ -24,9 +78,9 @@ function RemarkModal({ meeting }) {
         >
             <Modal.Header>備註</Modal.Header>
             <Modal.Content>
-                <div style={{display:'flex',flexDirection:'row'}}>
+                <div style={{ display: 'flex', flexDirection: 'row' }}>
                     <p>title: </p>
-                    <input type="text" />
+                    <input type="text" value={remark} onChange={(e) => { setRemark(e.target.value ?? '') }} />
                 </div>
             </Modal.Content>
             <Modal.Actions>
@@ -37,7 +91,7 @@ function RemarkModal({ meeting }) {
                     content="確認"
                     labelPosition='left'
                     icon='checkmark'
-                    onClick={() => setIsRemark(false)}
+                    onClick={setRemarkHandler}
                     positive
                 />
             </Modal.Actions>
